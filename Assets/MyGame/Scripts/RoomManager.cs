c2597bab9d50e8e6604031d18575ed57477d75d8
eoberndorfer <emil.oberndorfer08@htl-salzburg.ac.at>
2024-12-08 21:00:28 +0100

    updated#

--- Assets/MyGame/Scripts/RoomManager.cs
+++ Assets/MyGame/Scripts/RoomManager.cs
@@ -8,7 +8,6 @@ public class RoomManager : MonoBehaviour
     [SerializeField] private StudentData[] students;
     [SerializeField] private GameObject tablePrefab;
     [SerializeField] private GameObject chairPrefab;
-    [SerializeField] private GameObject twoSeater;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +16,20 @@ public class RoomManager : MonoBehaviour
             for (int col = 0; col < tableLayout.columns; col++)
             {
                 Vector3 tablePosition = new Vector3(col * tableLayout.tableSpacing, 0, row * tableLayout.tableSpacing);
-                GameObject table = Instantiate(twoSeater, tablePosition, Quaternion.identity, transform);
+                GameObject table = Instantiate(tablePrefab, tablePosition, Quaternion.identity, transform);
 
+                Transform pos1 = table.transform.Find("pos1");
+                Transform pos2 = table.transform.Find("pos2");
+
+                if (pos1)
+                {
+                    Instantiate(chairPrefab, pos1.position, pos1.rotation, table.transform);
+                }
+                if (pos2)
+                {
+                    Instantiate(chairPrefab, pos2.position, pos2.rotation, table.transform);
+                }
             }
         }
-
     }
 }
