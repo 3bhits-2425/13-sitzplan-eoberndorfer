abf1065151e0bcfc8f59f594ee1339c88e96da9a
eoberndorfer <emil.oberndorfer08@htl-salzburg.ac.at>
2024-12-10 19:04:23 +0100

    tried to fix but couldnt get humans to spawn at the correct place

--- Assets/MyGame/Scripts/RoomManager.cs
+++ Assets/MyGame/Scripts/RoomManager.cs
@@ -4,32 +4,79 @@ using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
-    [SerializeField] private TableStudentData tableLayout;
-    [SerializeField] private StudentData[] students;
-    [SerializeField] private GameObject tablePrefab;
-    [SerializeField] private GameObject chairPrefab;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TableStudentData tableLayout; 
+    [SerializeField] private StudentData[] students; 
+    [SerializeField] private GameObject tablePrefab; 
+    [SerializeField] private GameObject chairPrefab; 
+    private int studentIndex = 0; 
+
+
+    private void Start()
     {
         for (int row = 0; row < tableLayout.rows; row++)
         {
             for (int col = 0; col < tableLayout.columns; col++)
             {
                 Vector3 tablePosition = new Vector3(col * tableLayout.tableSpacing, 0, row * tableLayout.tableSpacing);
+
+               
                 GameObject table = Instantiate(tablePrefab, tablePosition, Quaternion.identity, transform);
 
+                
                 Transform pos1 = table.transform.Find("pos1");
                 Transform pos2 = table.transform.Find("pos2");
-
-                if (pos1)
+                if (pos1 != null)
                 {
                     Instantiate(chairPrefab, pos1.position, pos1.rotation, table.transform);
                 }
-                if (pos2)
+                if (pos2 != null)
                 {
                     Instantiate(chairPrefab, pos2.position, pos2.rotation, table.transform);
+                }
+
+                
+                Transform posh1 = table.transform.Find("posh1");
+                Transform posh2 = table.transform.Find("posh2");
+
+                if (posh1 != null)
+                {
+                    PlaceStudent(posh1.position, posh1.rotation, table.transform);
+                }
+                if (posh2 != null)
+                {
+                    PlaceStudent(posh2.position, posh2.rotation, table.transform);
                 }
             }
         }
     }
+
+    private void PlaceStudent(Vector3 position, Quaternion rotation, Transform parent)
+    {
+        if (studentIndex < students.Length)
+        {
+            GameObject studentPrefab = students[studentIndex].studentPrefab;
+
+            //Prüfen ob student prefab vorhanden ist
+            if (studentPrefab != null)
+            {
+                // Studenten instanzieren
+                GameObject student = Instantiate(studentPrefab, position, rotation, parent);
+                student.name = students[studentIndex].studentName;
+
+                // Renderer suchen 
+                Renderer renderer = student.GetComponentInChildren<Renderer>();
+                if (renderer != null)
+                {
+                    // Neues Material erstellen und Albedo-Textur setzen
+                    Material newMaterial = new Material(renderer.material);
+                    newMaterial.mainTexture = students[studentIndex].albedoTexture;
+                    renderer.material = newMaterial;
+                }
+            }
+
+            studentIndex++;
+        }
+    }
+
+
 }
--- Assets/MyGame/Scripts/StudentData.cs
+++ Assets/MyGame/Scripts/StudentData.cs
@@ -1,10 +1,15 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewStudent", menuName = "Sitzplan/StudentData")]
+[CreateAssetMenu(fileName = "NewStudent", menuName = "Seatplan/Student")]
 public class StudentData : ScriptableObject
 {
     public string studentName;
     public Color eyecolor;
     public Sprite studentImage;
     public AudioClip studentClip;
+    public GameObject studentPrefab;
+    public Texture2D albedoTexture;
 }
